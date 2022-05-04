affdab6dea312a9b02615c0a4cbc0679547987f0
Eduardo Becerra <beduardo.becerra19@gmail.com>
2022-05-03 18:48:29 -0600

    Se elimino código del método render del Board para refactorizarlo

--- src/index.js
+++ src/index.js
@@ -36,17 +36,8 @@ import './index.css';
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
